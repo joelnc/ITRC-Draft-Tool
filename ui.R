5661c382df6389fc454940e52730b24b1b397452
joelnc <joel.nipper@charlottenc.gov>
2017-09-18 15:37:29 -0400

    Added ui navbar tabPanel for pollutant results

--- ui.R
+++ ui.R
@@ -1,11 +1,15 @@
 library(shiny)
 
 ## Load, format, subset, WQD File
-itrcDataFull <- read.csv("toolExData2.csv", stringsAsFactors=FALSE,
+## itrcDataFull <- read.csv("toolExData2.csv", stringsAsFactors=FALSE,
+##                      sep=",", header=TRUE, check.names=FALSE)
+
+itrcDataFull <- read.csv("Component Sheet 6-12-17 doug H.csv", stringsAsFactors=FALSE,
                      sep=",", header=TRUE, check.names=FALSE)
 
 ## Subset the data to workable table size for now
-itrcData <- itrcDataFull[,c(1,2,5,8,11,12,13,14,15,16)]
+##itrcData <- itrcDataFull[,c(1,2,5,8,11,12,13,14,15,16)]
+itrcData <- itrcDataFull
 
 ## Hyperlink practice names placeholder info sheets
 itrcData$Practice <- paste0("<a href=", itrcData$doc, " target='blank' >",
@@ -49,63 +53,86 @@ itrcData <<- subset(itrcData, select=-c(doc))
 ## Define UI for applicaiton that draws a hist
 shinyUI(
     navbarPage(title="ITRC BMP Tool",
-               selected="Data",
-               br(),
+               tabPanel("Pollutant Filtering"),
                h2("**DRAFT/PROTOTYPE**", align="center"),
                h2("ITRC Stormwater BMP Applicability / Evaulation Tool"),
-               fluidRow(
-                   h3("Select Criteria", align="center"),
-                   column(6,
-                          wellPanel(style="background-color: #D2DDE4",
-                                    selectInput(
-                                    inputId="pollutants",
-                                    label="Select Pollutant(s): ",
-                                    choices=names(itrcData[2:4]),
-                                    multiple=TRUE,
-                                    selectize=TRUE)
-                                    )
-                          ),
-                   column(6,
-                          wellPanel(style="background-color: #D2DDE4;",
-                                    includeCSS("styles.css"),
-                                    h4("Does data need to meet these conditions? (Select...)"),
-                                    h5("(*If none selected, all practices will be shown)"),
-                                    fluidRow(
-                                        column(6,
-                                           checkboxInput(inputId="TPL",
-                                                             label="3rd Party Lab?",
-                                                             value=FALSE)
-                                           ),
-                                        column(6,
-                                               checkboxInput(inputId="refJ",
-                                                             label="Ref. Journ?",
-                                                             value=FALSE)
-                                               )
-                                    ),
-                                    fluidRow(
-                                        column(6,
-                                               checkboxInput(inputId="TPF",
-                                                         label="3rd Party Field",
-                                                         value=FALSE)
-                                               ),
-                                        column(6,
-                                               checkboxInput(inputId="IDB",
-                                                             label="Int. DB? ",
-                                                             value=FALSE)
-                                               )
-                                    ),
-                                    fluidRow(
-                                        column(6,
-                                               checkboxInput(inputId="Vend",
-                                                             label="Vendor Data?",
-                                                             value=FALSE)
-                                               )
-                                    )
-                                    )
-                          )
+               h3("Here is the tool..."),
+               sidebarLayout(
+                   sidebarPanel(
+                       h5("Select the pollutants of concern from the drop down box, below.  The table will update to show only Stormwater Practices that have been determined to have meaningfull removal potential for the selected pollutant(s)."),
+                       br(),
+
+                       selectInput(inputId="pollutants",
+                                   label="Select Pollutant(s): ",
+                                   choices=names(itrcData[2:37]),
+                                   multiple=TRUE,
+                                   selectize=TRUE),
+                       h5("How were pollutant removal potentials established?")
+                   ),
+                   mainPanel(
+                       DT::dataTableOutput("results")
+                       )
                ),
-               h6("Note: The table is currently populated with dummy data.  This is for functionality and testing purposes only.", color="red"),
-               hr(),
-               DT::dataTableOutput("results2")
+               tabPanel("Previous Version",
+                        selected="Data",
+                        br(),
+                        h2("**DRAFT/PROTOTYPE**", align="center"),
+                        h2("ITRC Stormwater BMP Applicability / Evaulation Tool"),
+                        fluidRow(
+                            h3("Select Criteria", align="center"),
+                            column(6,
+                                   wellPanel(style="background-color: #D2DDE4",
+                                                 selectInput(
+                                                     inputId="pollutants2",
+                                                     label="Select Pollutant(s): ",
+                                                     choices=names(itrcData[2:4]),
+                                                     multiple=TRUE,
+                                                     selectize=TRUE)
+                                             )
+                                   ),
+                            column(6,
+                                   wellPanel(style="background-color: #D2DDE4;",
+                                                 includeCSS("styles.css"),
+                                             h4("Does data need to meet these conditions? (Select...)"),
+                                             h5("(*If none selected, all practices will be shown)"),
+                                             fluidRow(
+                                                 column(6,
+                                                        checkboxInput(inputId="TPL",
+                                                                      label="3rd Party Lab?",
+                                                                      value=FALSE)
+                                                        ),
+                                                 column(6,
+                                                        checkboxInput(inputId="refJ",
+                                                                      label="Ref. Journ?",
+                                                                      value=FALSE)
+                                                        )
+                                             ),
+                                             fluidRow(
+                                                 column(6,
+                                                        checkboxInput(inputId="TPF",
+                                                                      label="3rd Party Field",
+                                                                      value=FALSE)
+                                                        ),
+                                                 column(6,
+                                                        checkboxInput(inputId="IDB",
+                                                                      label="Int. DB? ",
+                                                                      value=FALSE)
+                                                        )
+                                             ),
+                                             fluidRow(
+                                                 column(6,
+                                                        checkboxInput(inputId="Vend",
+                                                                      label="Vendor Data?",
+                                                                      value=FALSE)
+                                                        )
+                                             )
+                                             )
+                                   )
+                        ),
+                        h6("Note: The table is currently populated with dummy data.  This is for functionality and testing purposes only.", color="red"),
+                        hr(),
+                        DT::dataTableOutput("results3")
+                        )
                )
 )
+

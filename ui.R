7ba4d7abfe0e69446fcb48364cdcbea8ab0e82a6
joel nipper <joel.nipper@charlottenc.gov>
2017-09-19 13:51:17 -0400

    Messing wiht collapsable content

--- ui.R
+++ ui.R
@@ -27,22 +27,69 @@ shinyUI(
                h2("**DRAFT/PROTOTYPE**", align="center"),
                h2("ITRC Stormwater BMP Applicability / Evaulation Tool"),
                h3("Here is the tool..."),
-               sidebarLayout(
-                   sidebarPanel(
-                       h5("Select the pollutants of concern from the drop down box, below.  The table will update to show only Stormwater Practices that have been determined to have meaningfull removal potential for the selected pollutant(s)."),
-                       br(),
+               column(4,
+                      br(),
+                      wellPanel(
+                          selectizeInput(inputId="pollutants",
+                                         label="Select Pollutant(s): ",
+                                         choices=list(
+                                             Sediments=names(itrcData[c(2:6)]),
+                                             Metals=names(itrcData[c(7:21)]),
+                                             Nutrients=names(itrcData[c(23,24,27:30)]),
+                                             Other=names(itrcData[c(22,25,26,31:38)])),
+                                         multiple=TRUE),
+                          h5("Select the pollutants of concern from the drop down box, above.  The table will update to show only Stormwater Practices that have been determined to have meaningfull removal potential for the selected pollutant(s)."),
 
-                       selectInput(inputId="pollutants",
-                                   label="Select Pollutant(s): ",
-                                   choices=names(itrcData[2:37]),
-                                   multiple=TRUE,
-                                   selectize=TRUE),
-                       h5("How were pollutant removal potentials established?")
-                   ),
-                   mainPanel(
-                       DT::dataTableOutput("results")
-                   )
-               )
+
+                          HTML('<button data-toggle="collapse" data-target="#demo" class="button" style="vertical-align:middle"><span>Pollutant Removal Determinations </span></button>'),
+                          tags$div(id = 'demo',  class="collapse",
+                                       h4("Some words....")
+
+                          )
+                      ),
+                      absolutePanel(
+                          id="id", class="panel panel-default",
+                          HTML('<button data-toggle="collapse" data-target="#demo"><span>Collapsible</span></button>'),
+                          tags$div(id = 'demo',  class="collapse",
+                                   wellPanel(h4("Some words...."))
+                                   )
+                          )
+                      ),
+               column(8,
+                      DT::dataTableOutput("results")
+                      )
                )
 )
+
+
+
+## ## Define UI for applicaiton that draws a hist
+## shinyUI(
+##     navbarPage(title="ITRC BMP Tool",
+##                tabPanel("Pollutant Filtering"),
+##                includeCSS("styles.css"),
+##                h2("**DRAFT/PROTOTYPE**", align="center"),
+##                h2("ITRC Stormwater BMP Applicability / Evaulation Tool"),
+##                h3("Here is the tool..."),
+##                sidebarLayout(
+##                    sidebarPanel(
+##                        h5("Select the pollutants of concern from the drop down box, below.  The table will update to show only Stormwater Practices that have been determined to have meaningfull removal potential for the selected pollutant(s)."),
+##                        br(),
+
+##                        selectizeInput(inputId="pollutants",
+##                                    label="Select Pollutant(s): ",
+##                                    choices=list(
+##                                        Sediments=names(itrcData[c(2:6)]),
+##                                        Metals=names(itrcData[c(7:21)]),
+##                                        Nutrients=names(itrcData[c(23,24,27:30)]),
+##                                        Other=names(itrcData[c(22,25,26,31:38)])),
+##                                    multiple=TRUE),
+##                        h5("How were pollutant removal potentials established?")
+##                    ),
+##                    mainPanel(
+##                        DT::dataTableOutput("results")
+##                    )
+##                )
+##                )
+## )
 
